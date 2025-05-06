98a1b13f1d501e5baf0c7397e3cd9832add7ea84
ChristySchott <christyhauschild@gmail.com>
2025-05-06 17:08:05 -0300

    chore: initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import { defineConfig } from 'eslint/config'
+
+export default defineConfig([
+  { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
+  { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.node } },
+  tseslint.configs.recommended,
+  eslintPluginPrettierRecommended,
+  {
+    rules: {
+      'no-console': 'warn',
+      'no-debugger': 'error',
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  },
+])
