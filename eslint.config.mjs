4ef5d27328a711e9ca65030c90238f2c6af4eaca
ChristySchott <christyhauschild@gmail.com>
2025-05-07 17:56:11 -0300

    chore: add build script

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,13 +2,14 @@ import js from '@eslint/js'
 import globals from 'globals'
 import tseslint from 'typescript-eslint'
 import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
-import { defineConfig } from 'eslint/config'
+import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs,ts}'], plugins: { js }, extends: ['js/recommended'] },
   { files: ['**/*.{js,mjs,cjs,ts}'], languageOptions: { globals: globals.node } },
   tseslint.configs.recommended,
   eslintPluginPrettierRecommended,
+  globalIgnores(['dist']),
   {
     rules: {
       'no-console': 'warn',
